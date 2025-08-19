41c561018528d2c584fde253d7b66651029a33a7
senna <joaopedrorochasenna@gmail.com>
2025-08-19 00:19:43 -0300

    fix: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { App } from './app';
+import { BtnFormCrudWorkersComponent } from './btn-form-crud-workers/btn-form-crud-workers.component';
+import { CrudWorkersComponent } from './crud-workers/crud-workers.component';
 
-export const routes: Routes = [{ path: '', component: LoginComponent }];
+export const routes: Routes = [
+  { path: 'btn-form-crud-workers', component: BtnFormCrudWorkersComponent },
+  { path: 'crud-workers', component: CrudWorkersComponent },
+  { path: '', component: LoginComponent },
+];
