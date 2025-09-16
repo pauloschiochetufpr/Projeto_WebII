7596b14b8804c67c79c0415ab312ccbb4efe1213
Henrique Corrêa <hm82118@gmail.com>
2025-09-16 13:38:49 -0300

    Update jsontest.spec.ts

--- frontend/src/app/services/jsontest.spec.ts
+++ frontend/src/app/services/jsontest.spec.ts
@@ -1,23 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { JsonTestService } from './jsontest';
 
-import { JsontestComponent } from './jsontest';
+describe('JsonTestService', () => {
+  let service: JsonTestService;
+  let httpMock: HttpTestingController;
 
-describe('Jsontest', () => {
-  let component: JsontestComponent;
-  let fixture: ComponentFixture<JsontestComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [JsonTestService],
+    });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [JsontestComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(JsontestComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    service = TestBed.inject(JsonTestService);  
+    httpMock = TestBed.inject(HttpTestingController); 
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify(); 
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should load users from service', (done) => {
+    const mock = [{ name: 'A', date: '2025-09-01' }];
+
+    service.getUsers().subscribe(users => {
+      expect(users.length).toBe(1);
+      expect(users).toEqual(mock);
+      done();
+    });
+
+    const req = httpMock.expectOne('assets/data.json');
+    expect(req.request.method).toBe('GET');
+    req.flush(mock); 
   });
 });
