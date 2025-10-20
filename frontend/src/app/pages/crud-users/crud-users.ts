f4ceb508747eea57e74402d6278ca703e6661fd8
PedroMoreira25 <pedrohmsil92@gmail.com>
2025-10-19 21:44:00 -0300

    Revert "remoção do crud-users porque vou fazer cruds separados para os diferentes acessos"
    This reverts commit 2d06b98b8f8873b3ffb87a1604893c806f081add.

---
+++ frontend/src/app/pages/crud-users/crud-users.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-users',
+  imports: [],
+  templateUrl: './crud-users.html',
+  styleUrl: './crud-users.css'
+})
+export class CrudUsers {
+
+  
+
+
+}
