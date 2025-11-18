e04979cf2ccad5b7a8a8253e5bf019570e700266
PedroMoreira25 <pedrohmsil92@gmail.com>
2025-11-18 14:16:29 -0300

    editar-categoria ajustando com o local

--- frontend/src/app/pages/categoria-equipamento/editar-categoria/editar-categoria.component.ts
+++ frontend/src/app/pages/categoria-equipamento/editar-categoria/editar-categoria.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-editar-categoria',
@@ -7,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrl: './editar-categoria.component.css'
 })
 export class EditarCategoriaComponent {
+
+  
+}
+*/
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm, FormsModule } from '@angular/forms';
+import { CategoriaEquipamento } from '../../../models/solicitacao.model';
+import { CategoriaEquipamentoService } from '../../../services/categoria-equipamento';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-editar-categoria',
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './editar-categoria.html',
+  styleUrl: './editar-categoria.css'
+})
+export class EditarCategoria implements OnInit {
+  @ViewChild('formInserirCategoria') formInserirCategoria!: NgForm; 
+  categoria : CategoriaEquipamento = new CategoriaEquipamento();
+
+  constructor(
+    private categoriaEquipamentoService : CategoriaEquipamentoService,
+    private route : ActivatedRoute,
+    private router : Router
+  ){}
+
+  ngOnInit(): void {
+    let id = +this.route.snapshot.params['id'];
+    const dado = this.categoriaEquipamentoService.buscarPorId(id);
+    if (dado !== undefined){
+      this.categoria = dado
+    }else{
+      throw new Error ("Categoria não encontrada: id = " + id);
+    }
+  }
+
+  atualizar(): void{
+    if(this.formInserirCategoria.form.valid){
+      this.categoriaEquipamentoService.atualizar(this.categoria);
+      this.router.navigate(['/categoriaEquipamento'])
+    }
+  }
+
+
 
 }
